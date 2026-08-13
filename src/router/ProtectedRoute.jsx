ddf08bb2c986abd33d6ba189cc8de59b6247ab1c
Neyla <neylavillah30@gmail.com>
2026-08-13 20:44:29 +0700

    fix: harden admin route authorization

--- src/router/ProtectedRoute.jsx
+++ src/router/ProtectedRoute.jsx
@@ -5,8 +5,10 @@ import { authStorage } from '@/lib/authStorage'
 export default function ProtectedRoute() {
   const location = useLocation()
   const accessToken = authStorage.getAccessToken()
+  const user = authStorage.getUser()
 
-  if (!accessToken) {
+  if (!accessToken || user?.role !== 'admin') {
+    authStorage.clearSession()
     return <Navigate to={ROUTES.login} replace state={{ from: location }} />
   }
 
